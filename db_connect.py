f771c2c6e9bca37e955a287b0cd975b95e6579e5
Vinuzan Ratnasingam <VRatnasingam@spartaglobal.com>
2019-10-02 10:50:52 +0100

    introduction to pyodbc

--- db_connect.py
+++ db_connect.py
@@ -14,6 +14,33 @@ conn_nwdb = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+serv
 print(conn_nwdb)
 
 #Create a cursor
+#Allows us to execute raeadonly queries on the database
+cursor = conn_nwdb.cursor()
+
+#using .execute on cursor
+cursor.execute('SELECT * FROM Customers;')
+
+#Fetch rows from cursor - .fetchone()
+row = cursor.fetchone()
 
 
-#Fetch some data
+#.fetchall()
+        #This is bad because if databse is too large, it can crash system
+rows = cursor.execute('SELECT * FROM Customers').fetchall()
+#print(rows) if this is a list, then:
+
+rows = cursor.execute('SELECT * FROM Products').fetchall()
+#We can iterate
+for record in rows:
+    print(record.UnitPrice) #We can access the column of a specific record
+
+#However this is dangerous (limited amount of RAM), because we can clog our machine with too much data.
+    #We can use while loops to be more efficient.
+
+rows = cursor.execute('SELECT * FROM Products')
+
+while True:
+    record = rows.fetchone()
+    if record is None:
+        break
+    print(record.UnitPrice)
